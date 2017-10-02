9919580ea20450ed52f1bc555fe79ab3323f55ce
singhbikramjit <bikramjit.singh@dal.ca>
2017-10-02 12:49:55 -0300

    steps

--- task1.py
+++ task1.py
@@ -1,10 +1,16 @@
 def crime():
  # myfile=open("Crime.cvs",'r')
-  with open("Crime.cvs", 'r') as myfile:
+  with open("Crime.csv", 'r') as myfile:
    myfile.read()
-  for line in myfile:
-    for words in line:
-      print(words)
-
+   print(myfile)
+  
+  
+  for r in myfile:
+    crime_type =r[8]
+    crime_id = r[7]
+    dict={crime_id: crime_type}
+    print(dict)
+   #for words in line:
+    #  print(word)
 
 crime()
